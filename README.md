91984e94ba0249e8ff2374e017f033c27e7b3a6a
U+039b <git@0x39b.fr>
2018-01-09 18:58:51 +0100

    Add JSON output
    Signed-off-by: U+039b <git@0x39b.fr>

--- README.md
+++ README.md
@@ -18,16 +18,28 @@ pip install -r requirements.txt
 ```
 
 # Analyze an APK file
+
+## Usage
+```
+$ python exodus_analyze.py -h
+Usage: exodus_analyze.py [options] apk_file
+
+Options:
+  -h, --help            show this help message and exit
+  -t, --text            print textual report (default)
+  -j, --json            print JSON report
+  -o OUTPUT_FILE, --output=OUTPUT_FILE
+                        store JSON report in file (requires -j option)
+```
+
+## Text output
 ```
 python exodus_analyze.py my_apk.apk
 ```
 be sure to activate the Python `virtualenv` before running `exodus_analyze.py`.
 
-## Example of output
+### Example of text output
 ```
-loading the apk
-loading trackers signatures from Exodus
-decoding the apk
 === Informations
 - APK path: /tmp/tmp1gzosyt4/com.semitan.tan.apk
 - APK sum: 8e85737be6911ea817b3b9f6a80290b85befe24ff5f57dc38996874dfde13ba7
@@ -52,10 +64,50 @@ decoding the apk
  - Google DoubleClick
 ```
 
+## JSON output
+```
+python exodus_analyze.py -j [-o report.json] my_apk.apk
+```
+be sure to activate the Python `virtualenv` before running `exodus_analyze.py`.
+
+### Example of text output
+```json
+{
+  "trackers": [
+    {
+      "id": 70,
+      "name": "Facebook Share"
+    },
+    [...]
+  ],
+  "apk": {
+    "path": "com.johnson.nett.apk",
+    "checksum": "70b6f0d9df432c66351a587df7b65bea160de59e791be420f0e68b2fc435429f"
+  },
+  "application": {
+    "version_code": "15",
+    "name": "Nett",
+    "permissions": [
+      "android.permission.INTERNET",
+      "android.permission.ACCESS_NETWORK_STATE",
+      "android.permission.WRITE_EXTERNAL_STORAGE",
+      "android.permission.READ_PHONE_STATE",
+      "android.permission.READ_EXTERNAL_STORAGE",
+      "android.permission.WAKE_LOCK",
+      "com.google.android.c2dm.permission.RECEIVE",
+      "com.johnson.nett.permission.C2D_MESSAGE"
+    ],
+    "version_name": "1.1.12",
+    "libraries": [],
+    "handle": "com.johnson.nett"
+  }
+}
+```
+
 ## Pitfalls
 This tool uses `dexdump` and only provides `GNU/Linux x86_64` version of it.
 
-# Download an APK from εxodus instance
+# Download an APK from an εxodus instance
 Create `config.py` file in the project directory specifying:
 ```
 CONFIG = {
--- exodus_analyze.py
+++ exodus_analyze.py
@@ -1,9 +1,66 @@
-import sys
+import json
+import optparse
+
 from exodus_core.analysis.static_analysis import StaticAnalysis
 
 
+class AnalysisHelper(StaticAnalysis):
+    def create_json_report(self):
+        return {
+            'application': {
+                'handle': self.get_package(),
+                'version_name': self.get_version(),
+                'version_code': self.get_version_code(),
+                'name': self.get_app_name(),
+                'permissions': self.get_permissions(),
+                'libraries': self.get_libraries()
+            },
+            'apk': {
+                'path': self.apk_path,
+                'checksum': self.get_sha256(),
+            },
+            'trackers': [{'name': t.name, 'id': t.id} for t in self.detect_trackers()],
+        }
+
+
 if __name__ == '__main__':
-    analysis = StaticAnalysis(sys.argv[1])
+    parser = optparse.OptionParser('usage: %prog [options] apk_file')
+    parser.add_option('-t', '--text',
+                      dest = 'text_mode',
+                      action = 'store_true',
+                      default = True,
+                      help = 'print textual report (default)')
+    parser.add_option('-j', '--json',
+                      dest = 'json_mode',
+                      action = 'store_true',
+                      default = False,
+                      help = 'print JSON report')
+    parser.add_option('-o', '--output',
+                      dest = 'output_file',
+                      default = None,
+                      help = 'store JSON report in file (requires -j option)')
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error('incorrect number of arguments')
+
+    mode = 1
+    if options.json_mode:
+        output = '%s.json' % options.output
+        mode = 2
+
+    apk_file = args[0]
+
+    analysis = AnalysisHelper(apk_file)
     analysis.load_trackers_signatures()
-    analysis.print_apk_infos()
-    analysis.print_embedded_trackers()
+    if mode == 1:
+        analysis.print_apk_infos()
+        analysis.print_embedded_trackers()
+    else:
+        report = json.dumps(analysis.create_json_report(), indent = 2)
+        if options.output_file is not None:
+            with open(options.output_file, 'w') as out:
+                out.writelines(report)
+        else:
+            print(report)
