a14fc031219c7c12b2e9f32fc6fb38ae0e23a5e8
U+039b <git@0x39b.fr>
2018-03-16 12:30:14 +0100

    Add icon perceptual hash
    Signed-off-by: U+039b <git@0x39b.fr>

--- exodus_analyze.py
+++ exodus_analyze.py
@@ -12,6 +12,7 @@ class AnalysisHelper(StaticAnalysis):
                 'version_name': self.get_version(),
                 'version_code': self.get_version_code(),
                 'uaid': self.get_application_universal_id(),
+                'icon_phash': self.get_icon_phash(),
                 'name': self.get_app_name(),
                 'permissions': self.get_permissions(),
                 'libraries': self.get_libraries(),
