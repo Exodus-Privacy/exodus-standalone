656202362852e919220d1efe1d3b34dde1fc3ef4
pnu <simon@pruneau.info>
2019-05-15 18:23:24 +0200

    PEP8 compliance for exodus_analyse.py & gitignore

--- exodus_analyze.py
+++ exodus_analyze.py
@@ -1,6 +1,6 @@
 import json
-import sys
 import optparse
+import sys
 
 from exodus_core.analysis.static_analysis import StaticAnalysis
 
@@ -21,26 +21,34 @@ class AnalysisHelper(StaticAnalysis):
                 'path': self.apk_path,
                 'checksum': self.get_sha256(),
             },
-            'trackers': [{'name': t.name, 'id': t.id} for t in self.detect_trackers()],
+            'trackers': [
+                {'name': t.name, 'id': t.id} for t in self.detect_trackers()
+            ],
         }
 
 
 if __name__ == '__main__':
     parser = optparse.OptionParser('usage: %prog [options] apk_file')
-    parser.add_option('-t', '--text',
-                      dest = 'text_mode',
-                      action = 'store_true',
-                      default = True,
-                      help = 'print textual report (default)')
-    parser.add_option('-j', '--json',
-                      dest = 'json_mode',
-                      action = 'store_true',
-                      default = False,
-                      help = 'print JSON report')
-    parser.add_option('-o', '--output',
-                      dest = 'output_file',
-                      default = None,
-                      help = 'store JSON report in file (requires -j option)')
+    parser.add_option(
+        '-t', '--text',
+        dest='text_mode',
+        action='store_true',
+        default=True,
+        help='print textual report (default)'
+    )
+    parser.add_option(
+        '-j', '--json',
+        dest='json_mode',
+        action='store_true',
+        default=False,
+        help='print JSON report'
+    )
+    parser.add_option(
+        '-o', '--output',
+        dest='output_file',
+        default=None,
+        help='store JSON report in file (requires -j option)'
+    )
 
     (options, args) = parser.parse_args()
 
@@ -59,7 +67,7 @@ if __name__ == '__main__':
         analysis.print_apk_infos()
         analysis.print_embedded_trackers()
     else:
-        report = json.dumps(analysis.create_json_report(), indent = 2)
+        report = json.dumps(analysis.create_json_report(), indent=2)
         if options.output_file is not None:
             with open(options.output_file, 'w') as out:
                 out.writelines(report)
